5c0f5a9e5668c861833924ba2fcf20593906d4a8
sompodsign <sompodsign@gmail.com>
2022-03-19 16:15:34 +0600

    function updated to update cell value on a excel file

--- utils/excel_utils.py
+++ utils/excel_utils.py
@@ -2,31 +2,6 @@ import openpyxl
 import pandas as pd
 
 
-def getRowCount(file, sheetName):
-    workbook = openpyxl.load_workbook(file)
-    sheet = workbook[sheetName]
-    return sheet.max_row
-
-
-def getColCount(file, sheetName):
-    workbook = openpyxl.load_workbook(file)
-    sheet = workbook[sheetName]
-    return sheet.max_column
-
-
-def readData(file, sheetName, rowno, colno):
-    workbook = openpyxl.load_workbook(file)
-    sheet = workbook[sheetName]
-    return sheet.cell(rowno, colno).value
-
-
-def writeData(file, sheetName, rowno, colno, data):
-    workbook = openpyxl.load_workbook(file)
-    sheet = workbook[sheetName]
-    sheet.cell(rowno, colno).value = data
-    workbook.save(file)
-
-
 def read_data_from_excel(file, sheet_name):
     df = pd.read_excel(file, sheet_name)
     return df.to_dict(orient="records")[0]
