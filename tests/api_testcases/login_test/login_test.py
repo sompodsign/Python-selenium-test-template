90c77b4715114af325c2c0e9b985589e7513e045
sompodsign <sompodsign@gmail.com>
2022-03-23 18:55:46 +0600

    icon added for logs

--- tests/api_testcases/login_test/login_test.py
+++ tests/api_testcases/login_test/login_test.py
@@ -16,9 +16,9 @@ class TestUserLogin(ApiTestDataProvider):
         try:
             assert status_code == 201
             assert result['response']['message'] == "User logged in successfully"
-            logger.info(f"Successful user login_test with valid data")
+            logger.info(f"✔ Successful user login_test with valid data")
         except AssertionError as e:
-            logger.error(f"Failed user login_test with valid data test: {e}")
+            logger.error(f"✘ Failed user login_test with valid data test: {e}")
             assert False
 
     @allure.step('Fail sign in without username')
@@ -28,9 +28,9 @@ class TestUserLogin(ApiTestDataProvider):
         try:
             assert status_code == 400
             assert result['response']['message'][0] == "email should not be empty"
-            logger.info("Failed sign in without email")
+            logger.info("✔ Failed sign in without email")
         except AssertionError as e:
-            logger.error(f"Failed sign in without email test: {e}")
+            logger.error(f"✘ Failed sign in without email test: {e}")
             assert False
 
     @allure.step('Fail sign in without password')
@@ -40,9 +40,9 @@ class TestUserLogin(ApiTestDataProvider):
         try:
             assert status_code == 400
             assert result['response']['message'][0] == "password should not be empty"
-            logger.info("Failed sign in without password")
+            logger.info("✔ Failed sign in without password")
         except AssertionError as e:
-            logger.error(f"Failed sign in without password test: {e}")
+            logger.error(f"✘ Failed sign in without password test: {e}")
             assert False
 
     @allure.step('Fail sign in get request')
@@ -51,9 +51,9 @@ class TestUserLogin(ApiTestDataProvider):
         status_code = result['status_code']
         try:
             assert status_code == 404
-            logger.info("Failed sign in with get request")
+            logger.info("✔ Failed sign in with get request")
         except AssertionError as e:
-            logger.error(f"Signed in with get request test: {e}")
+            logger.error(f"✘ Signed in with get request test: {e}")
             assert False
 
     @allure.step('Fail sign in put request')
@@ -62,9 +62,9 @@ class TestUserLogin(ApiTestDataProvider):
         status_code = result['status_code']
         try:
             assert status_code == 404
-            logger.info("Failed sign in with put request")
+            logger.info("✔ Failed sign in with put request")
         except AssertionError as e:
-            logger.error(f"Signed in with put request test: {e}")
+            logger.error(f"✘ Signed in with put request test: {e}")
             assert False
 
     @allure.step('Fail sign in delete')
@@ -73,6 +73,6 @@ class TestUserLogin(ApiTestDataProvider):
         status_code = result['status_code']
         try:
             assert status_code == 404
-            logger.info("Failed sign in with delete request")
+            logger.info("✔ Failed sign in with delete request")
         except AssertionError as e:
-            logger.error(f"Signed in with delete request test: {e}")
+            logger.error(f"✘ Signed in with delete request test: {e}")
