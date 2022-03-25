28527900b33815d2f5a6b9f460aad6c90bcd82b4
sompodsign <sompodsign@gmail.com>
2022-03-25 20:54:04 +0600

    otp verification script updated

--- tests/api_testcases/verify_otp/verify_otp_test.py
+++ tests/api_testcases/verify_otp/verify_otp_test.py
@@ -1,58 +1,73 @@
-# import allure
-#
-#
-#
-# # @allure.step("This test verifies that OTP validation works properly")
-# # def test_successful_otp_verification():
-# #     otp_api = VerifyOtpApi("/auth/api/verify/otp")
-# #     verify_email_api = VerifyEmailApi("/auth/api/verify/email")
-# #     email_result = verify_email_api.send_otp(email)
-# #     email_response = email_result["status_code"]
-# #     assert email_response == 201
-# #     print("\n------Please wait a while until the OTP is sent to your email")
-# #     time.sleep(40)
-# #     otp = get_otp()
-# #     headers_with_token["Authorization"] = "Bearer " + email_result["response"]["data"]
-# #     otp_response = otp_api.submit_otp(payload={"otp": otp}, headers=headers_with_token)
-# #     response = otp_response['status_code']
-# #     assert response == 201
-#
-#
-# @allure.step("THis test verifies that OTP can't be validated with invalid code")
-# def test_otp_verification_with_invalid_otp():
-#     otp_api = VerifyOtpApi("/auth/api/verify/otp")
-#     otp_response = otp_api.submit_otp(invalid_otp)
-#     response = otp_response['status_code']
-#     assert response == 400
-#
-#
-# @allure.step('Failed verify otp with no token')
-# def test_026_verify_otp():
-#     otp_api = VerifyOtpApi("/auth/api/verify/otp")
-#     result = otp_api.post_request(payload=invalid_otp)
-#     status_code = result['status_code']
-#     assert status_code == 400
-#
-#
-# @allure.step('Failed verify otp with get request')
-# def test_027_verify_otp():
-#     otp_api = VerifyOtpApi("/auth/api/verify/otp")
-#     result = otp_api.get_request(headers=headers_with_token, payload=invalid_otp)
-#     status_code = result['status_code']
-#     assert status_code == 400
-#
-#
-# @allure.step('Failed verify otp with put request')
-# def test_028_verify_otp():
-#     otp_api = VerifyOtpApi("/auth/api/verify/otp")
-#     result = otp_api.put_request(headers=headers_with_token, payload=invalid_otp)
-#     status_code = result['status_code']
-#     assert status_code == 400
-#
-#
-# @allure.step('Failed verify otp with put request')
-# def test_029_verify_otp():
-#     otp_api = VerifyOtpApi("/auth/api/verify/otp")
-#     result = otp_api.delete_request()
-#     status_code = result['status_code']
-#     assert status_code == 404
+import time
+
+import allure
+from application_settings.api_application_settings import ApiTestApplicationSettingsProvider
+from data_provider.api_test_data_provider import ApiTestDataProvider
+from utils.logger import CustomLogger
+
+logger = CustomLogger('api_test').get_logger()
+
+
+class TestVerifyEmail(ApiTestDataProvider):
+
+    verify_otp_api = ApiTestApplicationSettingsProvider('/auth/api/verify/otp')
+    verify_email_api = ApiTestApplicationSettingsProvider('/auth/api/verify/otp')
+
+    @allure.step("This test verifies that OTP validation works properly")
+    def test_successful_otp_verification(self):
+        email_result = self.verify_email_api.post_request(self.get_registered_email())
+        email_response = email_result["status_code"]
+        assert email_response == 201
+        logger.info("Please wait a while for the email to be sent")
+        time.sleep(40)
+        otp = self.get_otp()
+        headers = self.verify_email_api.get_headers()
+        headers["Authorization"] = "Bearer " + email_result["response"]["data"]
+        otp_response = self.verify_otp_api.post_request(payload={"otp": otp}, headers=headers)
+        response = otp_response['status_code']
+        try:
+            assert response == 201
+            logger.info("OTP verification successful")
+        except AssertionError:
+            logger.error("OTP verification failed")
+
+
+
+    # @allure.step("THis test verifies that OTP can't be validated with invalid code")
+    # def test_otp_verification_with_invalid_otp():
+    #     otp_api = VerifyOtpApi("/auth/api/verify/otp")
+    #     otp_response = otp_api.submit_otp(invalid_otp)
+    #     response = otp_response['status_code']
+    #     assert response == 400
+    #
+    #
+    # @allure.step('Failed verify otp with no token')
+    # def test_026_verify_otp():
+    #     otp_api = VerifyOtpApi("/auth/api/verify/otp")
+    #     result = otp_api.post_request(payload=invalid_otp)
+    #     status_code = result['status_code']
+    #     assert status_code == 400
+    #
+    #
+    # @allure.step('Failed verify otp with get request')
+    # def test_027_verify_otp():
+    #     otp_api = VerifyOtpApi("/auth/api/verify/otp")
+    #     result = otp_api.get_request(headers=headers_with_token, payload=invalid_otp)
+    #     status_code = result['status_code']
+    #     assert status_code == 400
+    #
+    #
+    # @allure.step('Failed verify otp with put request')
+    # def test_028_verify_otp():
+    #     otp_api = VerifyOtpApi("/auth/api/verify/otp")
+    #     result = otp_api.put_request(headers=headers_with_token, payload=invalid_otp)
+    #     status_code = result['status_code']
+    #     assert status_code == 400
+    #
+    #
+    # @allure.step('Failed verify otp with put request')
+    # def test_029_verify_otp():
+    #     otp_api = VerifyOtpApi("/auth/api/verify/otp")
+    #     result = otp_api.delete_request()
+    #     status_code = result['status_code']
+    #     assert status_code == 404
